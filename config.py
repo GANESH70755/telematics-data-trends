eadfa3df22dbb1b2b69892a1c932006f9ec76459
GANESH70755 <104563834+GANESH70755@users.noreply.github.com>
2025-07-31 14:32:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,38 +1,11 @@
-
-# conn_params = {
-# "OPENAI_API_KEY" : "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",
-# "account" : "LG95411",
-# "region" : "central-india.azure",
-# "user" : "GANESHK",
-# "password" : "snowflakeK@9392",
-# "database" : "SNOWPILOT_DB",
-# "db_schema" : "SNOWPILOT_SCHEMA"
-# }
-
-
-# conn_params = {
-# "OPENAI_API_KEY" : "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",
-# "account" : "PX69115",
-# "region" : "east-us-2.azure",
-# "user" : "VRUSHANG",
-# "password" : "Vrushang@123",
-# "database" : "SNOWIOTB",
-# "db_schema" : "SNOWIOTB_SCHEMA"
-# }
-
+import os
 
 conn_params = {
-"OPENAI_API_KEY" : "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",
-"account" : "anblicksorg_aws",
-"region" : "us-east-1",
-"user" : "SNOWIOT",
-"password" : "SnowIOT@202308",
-"database" : "SNOWIOT_DB",
-"db_schema" : "MAIN"
+    "OPENAI_API_KEY": os.environ.get("OPENAI_API_KEY"),
+    "account": os.environ.get("SNOWFLAKE_ACCOUNT"),
+    "region": os.environ.get("SNOWFLAKE_REGION"),
+    "user": os.environ.get("SNOWFLAKE_USER"),
+    "password": os.environ.get("SNOWFLAKE_PASSWORD"),
+    "database": os.environ.get("SNOWFLAKE_DATABASE"),
+    "db_schema": os.environ.get("SNOWFLAKE_DB_SCHEMA")
 }
-
-
-# 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
-# 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
-
-# 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
